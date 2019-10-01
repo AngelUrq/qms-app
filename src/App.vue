145f882faee35a5b6a3b266ad49ef05b1e558110
AngelUrq <angel4441916@gmail.com>
2019-09-30 22:27:45 -0400

    dashboard template added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <core-app-bar />
+    <core-drawer />
+    <core-view />
+    <core-footer />
+  </v-app>
 </template>
+
+<script>
+export default {
+  components: {
+    CoreDrawer: () => import('@/components/core/Drawer'),
+    CoreFooter: () => import('@/components/core/Footer'),
+    CoreAppBar: () => import('@/components/core/AppBar'),
+    CoreView: () => import('@/components/core/View')
+  }
+}
+</script>
---
+++ src/components/core/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+#core-footer {
+  z-index: 0;
+}
+</style>
