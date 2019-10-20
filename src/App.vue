e3eeb307fac8ad83edd38014b45f044cb82cba4b
wowblast <jfrt142012@hotmail.com>
2019-10-20 17:02:24 -0400

    conditional rendering on login added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <core-view />
+    <core-app-bar  v-if='isVisible'/>
+    <core-drawer  v-if='isVisible'/>
+    <core-view/>
+    <core-footer  v-if='isVisible'/>
   </v-app>
 </template>
 
@@ -8,10 +11,19 @@
 
 export default {
   components: {
-    CoreView: () => import('@/components/core/View')
+    CoreView: () => import('@/components/core/View'),
+    CoreDrawer: () => import('@/components/core/Drawer'),
+    CoreFooter: () => import('@/components/core/Footer'),
+    CoreAppBar: () => import('@/components/core/AppBar')
   },
   created  () {
     this.$store.commit('saveToken', localStorage.getItem('token'))
+  },
+
+  computed: {
+    isVisible () {
+      return this.$route.path !== '/'
+    }
   }
 }
 window.addEventListener('beforeunload', function (e) {
