93755636e6a38cc4c0ed890c61ca6b18c7cb8056
Adriana <adrianaorellana34@gmail.com>
2019-10-24 00:11:50 -0400

    register report format added

--- src/views/RegisterReportFormat.vue
+++ src/views/RegisterReportFormat.vue
@@ -7,13 +7,13 @@
       <v-container>
         <v-row>
           <v-col cols="12">
-            <v-text-field label="Nombre" prepend-icon="mdi-clipboard-text" required></v-text-field>
+            <v-text-field label="Nombre" v-model="reportFormat.name" prepend-icon="mdi-clipboard-text" required></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-text-field label="Versión" required></v-text-field>
+            <v-text-field label="Versión" v-model="reportFormat.version" required></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-text-field label="Título" required></v-text-field>
+            <v-text-field label="Título" v-model="reportFormat.title" required></v-text-field>
           </v-col>
           <v-row class="pa-3" no-gutters>
             <h3>Subtítulos: </h3>
@@ -27,7 +27,7 @@
               </v-btn>
           </v-row>
         </v-row>
-        <v-row class="ml-3" v-for="(subtitle, i) in subtitles" :key="i" >
+        <v-row class="ml-3" v-for="(subtitle, i) in reportFormat.subtitles" :key="i" >
           <v-text-field solo class="mr-3" :value="subtitle"></v-text-field>
           <v-btn
           class="mr-5"
@@ -43,18 +43,50 @@
     </v-card-text>
     <v-card-actions>
       <v-row class="pa-3" justify="end" no-gutters>
-        <v-btn color="blue darken-1" text>Registrar</v-btn>
+        <v-btn color="blue darken-1" @click="register()" text>Registrar</v-btn>
       </v-row>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import axios from 'axios'
+import { backendURL } from '@/data.js'
 export default {
   data () {
     return {
       newSubtitle: '',
-      subtitles: ['Uno', 'Dos']
+      reportFormat: {
+        name: '',
+        version: '',
+        title: '',
+        subtitles: []
+      }
+    }
+  },
+  methods: {
+    register () {
+      var config = {
+        headers: {
+          'x-access-token': this.$store.state.token
+        }
+      }
+      var newReportFormat = {
+        name: this.reportFormat.name,
+        version: this.reportFormat.version,
+        creationDate: new Date(),
+        lastModificationDate: new Date(),
+        title: this.reportFormat.title,
+        subtitles: this.reportFormat.subtitles
+      }
+
+      axios.post(backendURL + '/api/report-format', config, newReportFormat)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(e => {
+          console.log('An exception has occurred: ' + e)
+        })
     }
   }
 }
