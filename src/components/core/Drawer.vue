3f9bc2d88ab45aea2cad5d4ff111dcd8219e64ff
Adriana <adrianaorellana34@gmail.com>
2019-11-24 14:01:01 -0400

    Drawer: action plan buttons added

--- src/components/core/Drawer.vue
+++ src/components/core/Drawer.vue
@@ -9,7 +9,7 @@
     floating
     mobile-break-point="991"
     persistent
-    width="260"
+    width="300"
   >
     <template v-slot:img="attrs">
       <v-img v-bind="attrs" gradient="to top, rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)" />
@@ -95,10 +95,21 @@ export default {
         ]
       },
       {
-        to: '/table-list',
         icon: 'mdi-clipboard-outline',
         text: 'Planes de acción',
-        containsSubitems: false
+        containsSubitems: true,
+        subitems: [
+          {
+            // Put route
+            to: '/',
+            text: 'Formatos'
+          },
+          {
+            // Put route
+            to: '/',
+            text: 'Gestor'
+          }
+        ]
       },
       {
         to: '/user-profile',
