156be82a40e14ea947a2695a4f64591d89361ca7
Angel <angel4441916@gmail.com>
2019-11-24 13:34:21 -0400

    merge

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,50 +1,55 @@
 <template>
   <div>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar dark color="indigo darken-4">
-            <v-toolbar-title>Inicio de sesión</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                prepend-icon="mdi-account-circle"
-                name="login"
-                label="E-mail"
-                id="login"
-                type="text"
-                v-model="User.emailOrCode"
-                color="amber accent-4"
-              ></v-text-field>
-              <v-text-field
-                prepend-icon="mdi-lock"
-                name="password"
-                label="Contraseña"
-                id="password"
-                type="password"
-                v-model="User.password"
-                color="amber accent-4"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions class="justify-center">
-            <v-btn  @click.prevent="loginUser" class="mb-3" dark color="indigo darken-1" rounded>Ingresar</v-btn>
-          </v-card-actions>
-        </v-card>
-        <v-alert
-        v-model="alert.error"
-          type="error"
-          >
-          {{ alert.message }}
-        </v-alert>
-      </v-flex>
-    </v-layout>
+    <section>
+      <v-container fill-height>
+        <v-layout justify-center align-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="indigo darken-4">
+                <v-toolbar-title>Inicio de sesión</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    prepend-icon="mdi-account-circle"
+                    name="login"
+                    label="Usuario"
+                    id="login"
+                    type="text"
+                    color="amber accent-4"
+                    v-model="User.emailOrCode"
+                  ></v-text-field>
+                  <v-text-field
+                    prepend-icon="mdi-lock"
+                    name="password"
+                    label="Contraseña"
+                    id="password"
+                    type="password"
+                    color="amber accent-4"
+                    v-model="User.password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="justify-center">
+                <v-btn class="mb-3" dark color="indigo darken-1" @click="loginUser">Ingresar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </section>
+    <div class="fullscreen-bg" v-if="production">
+      <video id="mi-video" loop autoplay muted class="fullscreen">
+        <source src="../assets/timelapse.mp4" type="video/mp4" />
+      </video>
+    </div>
   </div>
 </template>
+
 <script>
 import axios from 'axios'
-import { backendURL } from '@/data.js'
+
+import { backendURL, production } from '@/data.js'
 
 export default {
   data () {
@@ -56,7 +61,8 @@ export default {
       alert: {
         message: '',
         error: false
-      }
+      },
+      production: production
     }
   },
   methods: {
@@ -66,7 +72,7 @@ export default {
         code: this.User.emailOrCode,
         password: this.User.password
       }
-      console.log(requirements)
+
       axios.post(backendURL + '/api/users/signin', requirements)
         .then(response => {
           let authToken = response.data.token
@@ -85,3 +91,24 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.fullscreen {
+  background-size: cover;
+  background-position: center;
+  filter: brightness(0.7);
+  height: 100vh;
+  object-fit: fill;
+  transition: initial;
+  width: 100%;
+}
+
+section {
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  z-index: 10;
+}
+
+</style>
