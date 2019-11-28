d1ea9670c10869ac5c492596879665be707627bc
Angel <angel4441916@gmail.com>
2019-11-28 18:41:25 -0400

    user profile added

--- src/views/users/UserProfile.vue
+++ src/views/users/UserProfile.vue
@@ -1,160 +1,65 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-  >
+  <v-container fill-height fluid>
     <v-row justify="center">
-      <v-col
-        cols="12"
-        md="8"
-      >
-        <material-card
-          color="green"
-          title="Edit Profile"
-          text="Complete your profile"
-        >
+      <v-col cols="12" md="8">
+        <material-card color="indigo darken-2" title="Perfil de usuario" text="Consulta tu información">
           <v-form>
             <v-container class="py-0">
               <v-row>
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Company (disabled)"
-                    disabled
-                  />
+                <v-col cols="12" md="4">
+                  <v-text-field label="UPB" disabled />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    class="purple-input"
-                    label="User Name"
-                  />
+                <v-col cols="12" md="4">
+                  <v-text-field class="purple-input" label="Código" :value="user.code" readonly/>
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Email Address"
-                    class="purple-input"
-                  />
+                <v-col cols="12" md="4">
+                  <v-text-field label="Correo electrónico" class="purple-input" :value="user.email" readonly/>
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <v-text-field
-                    label="First Name"
-                    class="purple-input"
-                  />
+                <v-col cols="12" md="6">
+                  <v-text-field label="Nombres" class="purple-input" :value="user.firstNames" readonly/>
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <v-text-field
-                    label="Last Name"
-                    class="purple-input"
-                  />
+                <v-col cols="12" md="6">
+                  <v-text-field label="Apellidos" class="purple-input" :value="user.paternalLastName + ' ' + user.maternalLastName" readonly/>
                 </v-col>
 
                 <v-col cols="12">
-                  <v-text-field
-                    label="Adress"
-                    class="purple-input"
-                  />
+                  <v-text-field label="Teléfono" class="purple-input" :value="user.phone" readonly/>
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="City"
-                    class="purple-input"
-                  />
+                <v-col cols="12" md="4">
+                  <v-text-field label="Ciudad" class="purple-input" :value="user.city" readonly/>
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Country"
-                    class="purple-input"
-                  />
+                <v-col cols="12" md="4">
+                  <v-text-field label="País" class="purple-input" value="Bolivia" readonly/>
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    class="purple-input"
-                    label="Postal Code"
-                    type="number"
-                  />
-                </v-col>
-
-                <v-col cols="12">
-                  <v-textarea
-                    class="purple-input"
-                    label="About Me"
-                    value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  class="text-right"
-                >
-                  <v-btn color="success">
-                    Update Profile
-                  </v-btn>
+                <v-col cols="12" class="text-right">
+                  <v-btn color="indigo darken-2">Continua navegando</v-btn>
                 </v-col>
               </v-row>
             </v-container>
           </v-form>
         </material-card>
       </v-col>
-      <v-col
-        cols="12"
-        md="4"
-      >
+      <v-col cols="12" md="4">
         <material-card class="v-card-profile">
-          <v-avatar
-            slot="offset"
-            class="mx-auto d-block elevation-6"
-            size="130"
-          >
-            <img
-              src="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
-            >
+          <v-avatar slot="offset" class="mx-auto d-block elevation-6" size="130">
+            <img src="https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png" />
           </v-avatar>
           <v-card-text class="text-center">
-            <h6 class="overline mb-3">
-              CEO / CO-FOUNDER
-            </h6>
+            <h6 class="overline mb-3">{{ user.role }}</h6>
 
-            <h4 class="font-weight-light">
-              Alec Thompson
-            </h4>
+            <h4 class="font-weight-light">{{ user.firstNames }}</h4>
 
-            <p class="font-weight-light">
-              Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-            </p>
+            <p
+              class="font-weight-light"
+            >Si deseas cambiar la información de tu perfil contacta al administrador de esta aplicación</p>
 
-            <v-btn color="success">
-              Follow
-            </v-btn>
           </v-card-text>
         </material-card>
       </v-col>
@@ -163,7 +68,33 @@
 </template>
 
 <script>
+import axios from 'axios'
+
+import { backendURL } from '@/data.js'
+
 export default {
-  //
+  data: function () {
+    return {
+      user: {}
+    }
+  },
+  mounted: function () {
+    let config = { headers: { 'x-access-token': this.$store.state.token } }
+
+    axios
+      .get(backendURL + '/api/users/' + this.$store.state.token, config)
+      .then(response => {
+        this.user = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>
+
+<style scoped>
+.v-btn{
+  color:white;
+}
+</style>
