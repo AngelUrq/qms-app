d28bac43eab006e0870b3f69a93e17538ca743e3
Diego <dugotcharlot@gmail.com>
2019-12-04 19:09:56 -0400

    delete Action Plans & formats added

--- src/views/action-plan/Manager.vue
+++ src/views/action-plan/Manager.vue
@@ -70,9 +70,21 @@
                 icon
                 color="blue-grey lighten-1"
                 class="mr-1"
+                @click="showDeleteDialog = true"
               >
                 <v-icon>mdi-delete</v-icon>
               </v-btn>
+              <v-dialog v-model="showDeleteDialog" persistent max-width="400">
+                <v-card>
+                  <v-card-title class="headline">Eliminar</v-card-title>
+                  <v-card-text>¿Eliminar este plan de acción?</v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="pink lighten-3" text @click="deleteActionPlan(item)">Eliminar</v-btn>
+                    <v-btn color="pink lighten-3" text @click="showDeleteDialog = false">Cancelar</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </template>
           </v-data-table>
         </material-card>
@@ -98,6 +110,7 @@ export default {
     return {
       search: '',
       showEditActionPlanForm: false,
+      showDeleteDialog: false,
       headers: [
         {
           sortable: true,
@@ -152,6 +165,19 @@ export default {
       axios.get(backendURL + '/api/action-plans', config).then(response => {
         this.actionPlans = response.data
       })
+    },
+    deleteActionPlan (actionPlan) {
+      let config = { headers: { 'x-access-token': this.$store.state.token } }
+
+      axios.delete(backendURL + '/api/action-plans/' + actionPlan._id, config)
+        .then(response => {
+          console.log(response)
+          this.showDeleteDialog = false
+          this.updateList()
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
