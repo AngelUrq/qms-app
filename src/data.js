16a744c4eaf7303695519840acb70ddd7d2535e9
AngelUrq <angel4441916@gmail.com>
2020-02-10 16:18:02 -0400

    new server ip

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 let local = false // change this
 
-export const backendURL = local ? 'http://localhost:3000' : 'http://ec2-18-228-155-128.sa-east-1.compute.amazonaws.com:3000/'
+export const backendURL = local ? 'http://localhost:3000' : '54.94.206.159:3000/'
 export const production = true
 export const dashboardRoute = '/dashboard'
 export const loginRoute = '/login'
